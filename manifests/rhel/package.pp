6b4de1025bbe659460205fb7b48456ab02f5cadb
Wolf Noble <loiosh@kiera.nat.wolfspaw.com>
2012-12-30 03:46:10 -0600

    added package.pp
    Signed-off-by: Wolf Noble <loiosh@kiera.nat.wolfspaw.com>

---
+++ manifests/rhel/package.pp
@@ -0,0 +1,24 @@
+# == Class: murmur::rhel::package
+#  wrapper class
+#
+class murmur::rhel::package {
+  Package{} -> Anchor['murmur::package::end']
+  # end of variables
+  case $murmur::ensure {
+    present, enabled, active, disabled, stopped: {
+      #everything should be installed
+      package { 'Murmur':
+        ensure => 'present',
+      } -> Anchor['murmur::package::end']
+    }#end present case
+    absent: {
+      #everything should be removed
+      package { 'murmur':
+        ensure => 'absent',
+      } -> Anchor['murmur::package::end']
+    }#end absent case
+    default: {
+      notice "murmur::ensure has an unsupported value of ${murmur::ensure}."
+    }#end default ensure case
+  }#end ensure case
+}#end class

74f06a038bcbd93b8c6dab315c4369ee19ced223
Wolf Noble <loiosh@kiera.nat.wolfspaw.com>
2012-12-30 03:47:20 -0600

    added service
    Signed-off-by: Wolf Noble <loiosh@kiera.nat.wolfspaw.com>

---
+++ manifests/rhel/service.pp
@@ -0,0 +1,30 @@
+# == Class: murmur::rhel::service
+#  wrapper class
+Anchor['murmur::config::end'] -> Class['murmur::rhel::service']
+class murmur::rhel::service {
+  Service{} -> Anchor['murmur::service::end']
+  # end of variables
+  case $murmur::ensure {
+    enabled, active: {
+      #everything should be installed, but puppet is not managing the state of the service
+      service {'murmur':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['murmur_conf'],
+        require   => Package['murmur'],
+        hasstatus => true,
+      }#end service definition
+    }#end enabled class
+    disabled, stopped: {
+      service {'murmur':
+        ensure    => stopped,
+        enable    => false,
+        subscribe => File['murmur_conf'],
+        hasstatus => true,
+      }#end service definition
+    }#end disabled
+    default: {
+      #nothing to do, puppet shouldn't care about the service
+    }#end default ensure case
+  }#end ensure case
+}#end class
